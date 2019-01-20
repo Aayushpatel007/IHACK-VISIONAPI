d0d65bb82548ed811360318869bbc8c77b42cbfc
Aayush Patel <33193189+Aayushpatel007@users.noreply.github.com>
2019-01-20 07:29:06 +0530

    Update Constants.java

--- app/src/main/java/me/hammarstrom/imagerecognition/utilities/Constants.java
+++ app/src/main/java/me/hammarstrom/imagerecognition/utilities/Constants.java
@@ -5,7 +5,7 @@ package me.hammarstrom.imagerecognition.utilities;
  */
 public class Constants {
 
-    public static final String CLOUD_VISION_API_KEY = "AIzaSyBxFRHU5etlSSUGaSNm62Agv9Hn0gA_AR4";
+    public static final String CLOUD_VISION_API_KEY = "";
     public static final String APPLICATION_NAME = "";
 
 }
